d620c825345bf40599891fb4d12c801aa6876b62
opeyemi-jonah <ojonah@cnm.edu>
2022-06-28 06:09:21 -0600

    updated User columns based on ERD

--- onboard-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ onboard-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,24 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
+            $table->uuid('id');
+            $table->string('first_name',100);
+            $table->string('last_name',100);
+            $table->string('middle_name',100)->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('username')->unique();
+            $table->string('profile_picture',100)->nullable();
+            $table->dateTime('registeredAt')->default(new DateTime());
+            $table->string('phone_number', 11)->nullable();
+            $table->longText('bio')->nullable()->default('text');
+            $table->date('date_of_birth')->nullable();
+
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+
         });
     }
 
